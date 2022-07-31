a2d5043fc8ab54edcfd5d29518aa70cc252d0e47
신 우현 <woo_hyun_1@naver.com>
2022-07-31 23:47:37 +0900

    learn about document

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
-const age = parseInt(prompt("How old are you?"));
+const title=document.getElementById("title");
 
-if(isNaN(age) || age<0){
-    console.log("Please write a real positive number");
-}else if(age<18) {
-    console.log("You are too young");
-}else if(age>=18 && age<=50){
-    console.log("You can drink");
-}else if(age>50 && age<=80){
-    console.log("You should exercise");
-}else if(age>80){
-    console.log("You can do whatever you want.");
-}
+title.innerText="Got you!";
 
-// const calculator ={
-
-//     plus : function(a,b){
-//         return a+b;
-//     },
-
-//     minus : function(a,b){
-//         return a-b;
-//     },
-
-//     multiply : function(a,b){
-//         return a*b;
-//     },
-
-//     divide : function(a,b){
-//         return a/b;
-//     },
-
-//     power : function(a,b){
-//         return a**b;
-//     }
-
-// };
+console.log(title.id);
+console.log(title.className);
