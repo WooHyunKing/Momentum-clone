c6e8eec54fe17553901ba64f9d7e1a1a59b5a5dd
신 우현 <woo_hyun_1@naver.com>
2022-08-01 02:06:13 +0900

    learn about classlist

--- app.js
+++ app.js
@@ -1,53 +1,13 @@
 const h1=document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    //h1.style.color="blue"; style은 대부분 CSS에서 처리
-
-    const currentColor=h1.style.color;
-    let newColor;
-
-    if(currentColor==="blue"){
-        newColor="tomato";
+    const clickedClass="clicked";
+    if(h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     }
     else{
-        newColor="blue";
+        h1.classList.add(clickedClass);
     }
-
-    h1.style.color=newColor;
-}
-
-function handleMouseEnter(){
-    console.log("mouse is entered!");
-    h1.innerText="Mouse is here!";
-}
-
-function handleMouseLeave(){
-    console.log("mouse is leaved!");
-    h1.innerText="Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor="tomato"
-}
-
-function handleWindowCopy(){
-    alert("copied!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI!");
-}
-
-function handleWindowOnline(){
-    alert("WIFI is connected!");
 }
 
 h1.addEventListener("click",handleTitleClick);
-//h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave",handleMouseLeave);
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("online",handleWindowOnline);
