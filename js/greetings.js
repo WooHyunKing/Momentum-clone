0aa24b9a79348948f36b312de983070f4a6148d8
신 우현 <woo_hyun_1@naver.com>
2022-08-06 19:17:17 +0900

    learn about setInterval

--- js/greetings.js
+++ js/greetings.js
@@ -13,10 +13,11 @@ function onLoginSubmit(event){
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username=loginInput.value;
     localStorage.setItem(USERNAME_KEY,username);
-    paintGreetings(username);
+    paintGreetings();
 }
 
-function paintGreetings(username){
+function paintGreetings(){
+    const username=localStorage.getItem(USERNAME_KEY);
     greeting.innerText=`Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -29,5 +30,5 @@ if(savedUsername===null){
     loginForm.addEventListener("submit",onLoginSubmit);
 }else{
     //show the greeting
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
