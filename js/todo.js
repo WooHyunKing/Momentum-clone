972ab88573d88a14383546ed0505ec094208715d
신 우현 <woo_hyun_1@naver.com>
2022-08-10 15:58:24 +0900

    Saving ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm=document.getElementById("todo-form");
 const toDoInput=toDoForm.querySelector("input");
 const toDoList=document.getElementById("todo-list");
 
+const toDos=[];
+
+function saveToDos(){ //toDos 배열을 로컬저장소에 저장
+    localStorage.setItem("todos",JSON.stringify(toDos)); //stringify는 자바스크립트의 객체나 배열 등 모든 코드를 문자열로 만듬
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -25,9 +31,11 @@ function paintToDo(newTodo){
 
 function onToDoSubmit(event){
     event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value="";
-    paintToDo(newTodo);
+    const newTodo = toDoInput.value; //입력값 저장
+    toDoInput.value=""; //텍스트 초기화(지우기)
+    toDos.push(newTodo); //toDos 배열에 추가(push)
+    paintToDo(newTodo); //todo 출력
+    saveToDos(); //toDo 로컬저장소에 저장
 }
 
 toDoForm.addEventListener("submit",onToDoSubmit);
